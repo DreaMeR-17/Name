1d071d26045b9dafef7eeb5171f98dde6d2c3b27
DreaMeR <DreaMeR@DREAMER>
2024-07-16 11:13:58 +0600

    Добавьте файлы проекта.

---
+++ Name/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Name
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char symbol;
+
+            string name;
+            string middleLine;
+            string frame = "";
+
+            Console.WriteLine("Введите имя: ");
+            name = Console.ReadLine();
+
+            Console.WriteLine("Введите символ для рамки: ");
+            symbol = Console.ReadKey(true).KeyChar;
+
+            middleLine = symbol + name + symbol;
+
+            for (int i = 0; i < middleLine.Length; i++)
+            {
+                frame += symbol;
+            }
+
+            Console.WriteLine(frame + "\n" + middleLine + "\n" + frame);
+        }
+    }
+}
